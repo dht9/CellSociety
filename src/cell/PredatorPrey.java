227475bf6128acefb5ce491f2ad0fd4a927e5bea
Estelle He <wh103@duke.edu>
2017-09-23 17:55:18 -0400

    add neighborcell class and changes accordingly in other class in cell package, almost finish predatorprey

--- src/cell/PredatorPrey.java
+++ src/cell/PredatorPrey.java
@@ -1,6 +1,7 @@
 package cell;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * Game of Life implementation of Cell superclass
@@ -9,54 +10,110 @@ import java.util.ArrayList;
  *
  */
 public class PredatorPrey extends Cell{
+	private static final int FISH = 1;
+	private static final int SHARK = 2;
+	private static final int FOURADJACENT = 4;
+	
 	private double fishBreed;
 	private double sharkBreed;
 	private double fishDie;
 	private double sharkDie;
-	private int breedCount = 0;
-	private int dieCount = 0;
+	private int myBreedCount = 0;
+	private int myDieCount = 0;
+	private boolean myIsBreed = false;
+	private boolean myIsDie = false;
+	private double[] myparaList;
 
+	/**
+	 * constructor for predatorprey cell
+	 * @param row
+	 * @param column
+	 * @param state
+	 * @param gridSize
+	 * @param paraList
+	 */
 	public PredatorPrey(int row, int column, int state, int[] gridSize, double[] paraList) {
 		super(row, column, state, gridSize, paraList);
+		myparaList = paraList;
 		fishBreed = paraList[0];
 		sharkBreed = paraList[1];
 		fishDie = paraList[2];
 		sharkDie = paraList[3];
+		myNeighborCell = new NeighborCell(FOURADJACENT, true, this);
 	}
 
 	@Override
 	public void updateInfo(ArrayList<Cell> neighborlist) {
-		// TODO Auto-generated method stub
-		
+		myDieCount++;
+		myBreedCount++;
+		if (mystate == FISH) {
+			fishUpdate(neighborlist);
+		}
+		else if(mystate == SHARK) {
+			sharkUpdate(neighborlist);
+		}
 	}
 	
 	@Override
-	public void update() {
-		
+	public void update(ArrayList<Cell> cellList) {
+		if (myIsDie) {
+			cellList.remove(this);
+		}
+		if (myIsBreed) {
+			Cell baby = new PredatorPrey(myrow, mycol, mystate, mygrid, myparaList);
+			cellList.add(baby);
+		}
+		super.update(cellList);
 	}
-
-	@Override
-	public boolean isNeighbor(Cell other) {
-		if (other.equals(this)) {			//check not self
-			return false;
+	
+	/**
+	 * updateinfo implementation for fish 
+	 * 
+	 * @param neighborlist
+	 */
+	private void fishUpdate(ArrayList<Cell> neighborlist) {
+		if (myDieCount == fishDie) {
+			myIsDie = true;
 		}
-		if (Math.abs(other.row() - this.row()) <= 1 && other.column() == this.column()) {			//check up down
-			return true;
-		}
-		else if (Math.abs(other.column() - this.column()) <= 1 && other.row() == this.row()) {	//check left right
-			return true;
-		}
-		else if (this.isEdge()) {			//check torus
-			if (other.row()+this.row() == this.mygrid[0]-1 && other.column() == this.column() && 
-					Math.abs(other.row()-this.row()) == this.mygrid[0]) {
-				return true;
+		ArrayList<int[]> movablePos = emptyNeighbor(neighborlist);
+		int posSize = movablePos.size();
+		if (posSize != 0) {
+			int[] nextPos = movablePos.get((int) (Math.random()*(posSize-1)));
+			mynextRow = nextPos[0];
+			mynextCol = nextPos[1];
+			if (myBreedCount == fishBreed) {
+				myIsBreed = true;
+				myBreedCount = 0;
 			}
-			else if (other.column()+this.column() == this.mygrid[1]-1 && other.row() == this.row() && 
-					Math.abs(other.column()-this.column()) == this.mygrid[1]) {
-				return true;
-			}
 		}
-		return false;			//to make code readable, I did not collapse all if statement into one return value 
 	}
-
+	
+	private void sharkUpdate(ArrayList<Cell> neighborlist) {
+		Iterator<Cell> neighborIter = neighborlist.iterator();
+		ArrayList<Cell> availableFish = new ArrayList<Cell>();
+		ArrayList<int[]> movablePos = emptyNeighbor(neighborlist);
+		if (neighborIter.hasNext()) {
+			Cell neighbor = neighborIter.next();
+			if (neighbor.state() == FISH) {
+				availableFish.add(neighbor);
+			}
+		}
+		int fishSize = availableFish.size();
+		int posSize = movablePos.size();
+		if (fishSize != 0) {
+			PredatorPrey food = (PredatorPrey) availableFish.get((int) (Math.random()*(fishSize-1)));
+			food.consume();
+			this.mynextCol = food.column();
+			this.mynextRow = food.row();
+		}
+		else if(posSize != 0) {
+			int[] nextPos = movablePos.get((int) (Math.random()*(posSize-1)));
+			mynextRow = nextPos[0];
+			mynextCol = nextPos[1];
+		}
+	}
+	
+	public void consume() {
+		myIsDie = true;
+	}
 }
